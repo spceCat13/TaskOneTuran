979c9ec0d0845c2f3585d181cc40c5b00f6c3a6b
Ivan Kuzmin <spceCat1431@gmail.com>
2025-09-09 12:43:02 +0500

    first commit.

---
+++ src/main/java/com/punpun/Main.java
@@ -0,0 +1,12 @@
+package com.punpun;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println(MathOpers.subtraction(4,13));
+        System.out.println(MathOpers.division(4,13));
+        System.out.println(MathOpers.addition(4,13));
+        System.out.println(MathOpers.multiplication(4,13));
+    }
+}
---
+++ src/main/java/com/punpun/MathOpers.java
@@ -0,0 +1,35 @@
+package com.punpun;
+
+/*
+Написать программу, которая будет выполнять простые математические операции
+(сложение, вычитание, умножение и деление) на двух числах.
+*/
+
+
+public class MathOpers
+{
+    // Сложение
+    public static int addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    // Вычитание, вычитаем из первого числа второе.
+    public static int subtraction(int a, int b)
+    {
+        return a - b;
+    }
+
+    // Умножение
+    public static int multiplication(int a, int b)
+    {
+        return a * b;
+    }
+
+    // Деление, делим первое число на второе.
+    public static int division(int a, int b)
+    {
+
+        return a / b;
+    }
+}
